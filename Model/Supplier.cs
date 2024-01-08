8e0c42f731aac6cd833e183b5925314068e43fb1
Basura Jayanidu <106040232+AGBJayanidu@users.noreply.github.com>
2024-01-08 09:32:44 +0530

    Create Supplier.cs

---
+++ Model/Supplier.cs
@@ -0,0 +1,7 @@
+public class Supplier
+{
+    public Guid SupplierId { get; set; }
+    public string? SupplierName { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public bool IsActive { get; set; }
+}
